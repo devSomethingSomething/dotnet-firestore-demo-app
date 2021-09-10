c7bc8ce080e317dc24df5b40b16221ccab0d8107
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-09-10 16:52:01 +0200

    Add initial project files

---
+++ DotnetFirestoreDemoApp.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetFirestoreDemoApp.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
